4cfeeb71a762e33aabfb612be823f9f88fc684e6
Lucas <lucasvmorales@protonmail.com>
2023-07-22 14:16:46 -0300

    Classifying data

--- index.php
+++ index.php
@@ -7,26 +7,29 @@ use Phpml\Regression\LeastSquares;
 use Phpml\CrossValidation\StratifiedRandomSplit;
 use Phpml\Regression\SVR;
 use Phpml\Metric\Accuracy;
+use Phpml\Classification\KNearestNeighbors;
 
 require "vendor/autoload.php";
 
-$data = new CsvDataset("data/wine.csv", 13, true);
+$data = new CsvDataset("data/iris.csv", 4, true);
 
 // $dataSet = new RandomSplit($data, 0.2, 156);
 $dataSet = new RandomSplit($data, 0.2, 156);
 
 // $regression = new LeastSquares();
-$regression = new SVR();
-$regression->train($dataSet->getTrainSamples(), $dataSet->getTrainLabels());
+// $regression = new SVR();
+$classification = new KNearestNeighbors(3);
+$classification->train($dataSet->getTrainSamples(), $dataSet->getTrainLabels());
 
-$predict = $regression->predict($dataSet->getTestSamples());
+$predict = $classification->predict($dataSet->getTestSamples());
 
-$score = Regression::r2Score($dataSet->getTestLabels(), $predict);
-echo "R2 Score: " . $score . "\n";
+// $score = Regression::r2Score($dataSet->getTestLabels(), $predict);
+// echo "R2 Score: " . $score . "\n";
 
-foreach ($predict as &$target) {
-    $target = round($target, 0);
-}
+// foreach ($predict as &$target) {
+//     $target = round($target, 0);
+// }
 
 $accurace = Accuracy::score($dataSet->getTestLabels(), $predict);
 echo "Accuracy: " . $accurace . "\n";
+
